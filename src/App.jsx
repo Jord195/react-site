bb5e0ac7a502090b5c8f48e62fd5c3c22b60060e
Jordan Robins <jordan.robins@pertemps.co.uk>
2024-01-05 15:18:30 +0000

    Upgrade hashrouter to browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/home";
-import { About } from "./pages/about";
-import { ServicesPage } from "./pages/services";
-import { TestimonialsPage } from "./pages/testimonials";
-import { ContactUs } from "./pages/contact";
-import './App.css'
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/home';
+import { About } from './pages/about';
+import { ServicesPage } from './pages/services';
+import { TestimonialsPage } from './pages/testimonials';
+import { ContactUs } from './pages/contact';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <Router>
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/About" element={<About/>}/>
-            <Route path="/Services" element={<ServicesPage/>}/>
-            <Route path="/Testimonials" element={<TestimonialsPage/>}/>
-            <Route path="/Contact" element={<ContactUs/>}/>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Services" element={<ServicesPage />} />
+        <Route path="/Testimonials" element={<TestimonialsPage />} />
+        <Route path="/Contact" element={<ContactUs />} />
+      </Routes>
     </Router>
-)
+  );
 }
 
-export default App
+export default App;
