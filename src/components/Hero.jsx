7a831cd0645ce7a23f538ad3e6641fa1c56c87a8
Jord195 <128794365+Jord195@users.noreply.github.com>
2024-03-05 16:09:14 +0000

    hero font update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
             <div class="absolute z-10 h-full w-full bg-gradient-to-b from-black to-[#000000aa] opacity-70"></div>
             <div class="relative bg-mower-hero bg-center xs:h-[500px] md:h-[900px]">
                 <div class="relative z-30 flex h-full flex-col items-center justify-center gap-y-12 text-white">
-                <h1 class="text-center font-semibold xs:text-6xl md:text-9xl flex flex-col gap-6">
+                <h1 class="text-center font-semibold xs:text-6xl md:text-9xl flex flex-col xs:mt-8 sm:mt-0">
                     <span>Livingstone</span>
                     <span>Garden Services</span>
                 </h1>
